aa5b5d1e7644903a290058e36788063355155c4d
LonG <bunlongly03@gmail.com>
2024-05-07 15:14:45 +0700

    Creating a Redux Store

---
+++ client/src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { thunk } from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  });
+
+export default store;

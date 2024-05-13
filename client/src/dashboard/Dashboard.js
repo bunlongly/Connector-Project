f0eeb84bef423646a54ba4923b9672f4e0b47036
LonG <bunlongly03@gmail.com>
2024-05-13 11:55:03 +0700

    List Education & Experiences

--- client/src/dashboard/Dashboard.js
+++ client/src/dashboard/Dashboard.js
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../components/layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 import { getCurrentProfile } from '../actions/profile';
 import { Link } from 'react-router-dom';
 
-const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, [getCurrentProfile]);
@@ -21,12 +27,14 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
       </p>
       {profile != null ? (
         <Fragment>
-          <DashboardActions/>
+          <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet setup a profile, please add some info</p>
-          <Link to="/create-profile" className='btn btn-primary my-1'>  
+          <Link to='/create-profile' className='btn btn-primary my-1'>
             Create Profile
           </Link>
         </Fragment>

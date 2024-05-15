dab839582ccb95d5e31e10c31cdbbdb97950c84d
LonG <bunlongly03@gmail.com>
2024-05-15 11:22:53 +0700

    Profile Experience & Education Display

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import ProfileTop from './ProfileTop'
-import ProfileAbout from './ProfileAbout'
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation'
 import { getProfileById } from '../../actions/profile';
 import { useParams } from 'react-router-dom';
+
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
   const { id } = useParams();
@@ -35,10 +38,27 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
                 Edit Profile
               </Link>
             )}
-            <div className='profile-grid my-1'>
-            <ProfileTop profile={profile}/>
-            <ProfileAbout profile={profile}/>
+          <div className='profile-grid my-1'>
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (<Fragment>
+                {profile.experience.map(experience => (
+                    <ProfileExperience key={experience._id} experience={experience} />
+                ))}
+              </Fragment>) : (<h4>No experience credentials</h4>)}
             </div>
+
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (<Fragment>
+                {profile.education.map(education => (
+                    <ProfileEducation key={education._id} education={education} />
+                ))}
+              </Fragment>) : (<h4>No education credentials</h4>)}
+            </div>
+          </div>
         </Fragment>
       )}
     </Fragment>
@@ -48,12 +68,12 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 Profile.propTypes = {
   getProfileById: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
   profile: state.profile,
-  auth: state.auth,
+  auth: state.auth
 });
 
 export default connect(mapStateToProps, { getProfileById })(Profile);

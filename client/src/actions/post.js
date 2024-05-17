06288f9b58cd915fa4422b825dc4d4f4c5d2e860
LonG <bunlongly03@gmail.com>
2024-05-17 14:05:11 +0700

    Adding Posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { ADD_POST, DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -53,22 +53,50 @@ export const removeLike = postId => async dispatch => {
   }
 };
 
+
 //Delete post
 export const deletePost = postId => async dispatch => {
-  try {
-    const res = await axios.put(`/api/posts/${postId}`);
+    try {
+      await axios.delete(`/api/posts/${postId}`);
+  
+      dispatch({
+        type: DELETE_POST,
+        payload: postId
+      });
+  
+      dispatch(setAlert('Post Removed', 'success'));
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+  
 
-    dispatch({
-      type: DELETE_POST,
-      payload: postId
-    });
+//Add Post
+export const addPost = formData => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
 
-    dispatch(setAlert('Post Removed', 'success'));
-  } catch (err) {
-    dispatch({
-      type: POST_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status }
-    });
-  }
-};
+    try {
+      const res = await axios.post('/api/posts/', formData, config);
+  
+      dispatch({
+        type: ADD_POST,
+        payload: res.data
+      });
+  
+      dispatch(setAlert('Post Created', 'success'));
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+  
 
